0499615f2038bb03c777cc3301409e7f15294473
mxm1512 <maximpb@gmx.net>
2018-09-25 01:12:50 +0200

    ci3

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-htmlfile-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -20,12 +21,31 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'html'],
+    htmlReporter: {
+      outputFile: 'tests/units.html',
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromiumHeadless', 'ChromiumNoSandbox'],
+    customLaunchers: {
+      ChromiumHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+        ],
+        debug: true,
+      },
+      ChromiumNoSandbox: {
+        base: 'ChromiumHeadless',
+        flags: ['--no-sandbox', '--disable-translate', '--disable-extensions'],
+      },
+    },
     singleRun: false
   });
 };
